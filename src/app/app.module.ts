b3cd89f4130cd748cf979f2104f077ff5c74202c
HelderBarbosa <helderlb@gmail.com>
2019-04-05 07:30:25 -0300

    inicital

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
+  ],
+  exports: [
+    BsDropdownModule,
+     TooltipModule, 
+     ModalModule
+    ],
+  providers: [BsDropdownModule,,TooltipModule,ModalModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
